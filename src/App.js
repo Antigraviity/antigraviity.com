8aae54d0f78588531bf8f3f76c32244489d5cc8f
unntangle <unntangle20@gmail.com>
2025-12-04 23:31:40 +0530

    ADT 4

--- src/App.js
+++ src/App.js
@@ -1060,7 +1060,7 @@ const HeroSection = () => {
               backgroundClip: 'text',
             }}
           >
-            Elevating Businesses.
+            Elevating B.
           </p>
         </div>
 
