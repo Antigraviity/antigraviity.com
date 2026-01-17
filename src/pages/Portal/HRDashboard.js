4879a2429c81fce82f8bbfbd73575e59eb785215
unntangle <unntangle20@gmail.com>
2026-01-17 20:38:00 +0530

    Fix Vercel build: Remove unused XLSX import

--- src/pages/Portal/HRDashboard.js
+++ src/pages/Portal/HRDashboard.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import OfferLetter from '../../components/OfferLetter';
-import * as XLSX from 'xlsx';
 
 const HRDashboard = () => {
     const API_BASE_URL = 'http://localhost:5000';
